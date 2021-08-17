d17e9af078d9d0231a416ec4b330828ecc5c113a
Afonso Lage <lage.afonso@gmail.com>
2021-08-17 20:38:59 -0300

    Added voxel debugging draw

--- src/world/debug.rs
+++ src/world/debug.rs
@@ -1,4 +1,7 @@
-use bevy::{prelude::*, render::{pipeline::PrimitiveTopology, render_graph::base::MainPass}};
+use bevy::{
+    prelude::*,
+    render::{pipeline::PrimitiveTopology, render_graph::base::MainPass},
+};
 
 use super::*;
 
@@ -6,9 +9,42 @@ pub struct WireframeDebugPlugin;
 
 impl Plugin for WireframeDebugPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup_wireframe_shader)
-            .add_system(toggle_mesh_wireframe);
+        app.insert_resource(DebugWireframeState::default())
+            .add_startup_system(setup_wireframe_shader)
+            .add_system(toggle_mesh_wireframe)
+            .add_system(draw_chunk_voxels)
+            .add_system(delete_chunk_voxels)
+            .add_system(toggle_voxel_wireframe);
     }
+}
+
+#[derive(Default)]
+struct DebugWireframeState {
+    voxel_wireframe: bool,
+}
+
+fn toggle_voxel_wireframe(
+    mut commands: Commands,
+    keyboard: Res<Input<KeyCode>>,
+    mut wireframe_state: ResMut<DebugWireframeState>,
+    q_add: Query<Entity, (With<ChunkTypes>, Without<DrawVoxels>)>,
+    q_remove: Query<Entity, With<DrawVoxels>>,
+) {
+    if !keyboard.just_pressed(KeyCode::F2) {
+        return;
+    }
+
+    if wireframe_state.voxel_wireframe {
+        for e in q_remove.iter() {
+            commands.entity(e).remove::<DrawVoxels>();
+        }
+    } else {
+        for e in q_add.iter() {
+            commands.entity(e).insert(DrawVoxels);
+        }
+    }
+
+    wireframe_state.voxel_wireframe = !wireframe_state.voxel_wireframe;
 }
 
 struct WireframePipeline(Handle<PipelineDescriptor>);
@@ -114,7 +150,7 @@ fn compute_wireframe_indices(vertex_count: usize) -> Vec<u32> {
 
         res.push(i + 2);
         res.push(i + 3);
-        
+
         res.push(i + 3);
         res.push(i);
 
@@ -122,4 +158,80 @@ fn compute_wireframe_indices(vertex_count: usize) -> Vec<u32> {
     }
 
     res
+}
+
+pub struct DrawVoxels;
+struct DrawVoxelDone(Entity);
+
+fn draw_chunk_voxels(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    wireframe_pipeline_handle: Res<WireframePipeline>,
+    q: Query<(Entity, &ChunkTypes), Added<DrawVoxels>>,
+) {
+    for (e, types) in q.iter() {
+        let (vertices, indices) = generate_voxel_edges_mesh(&types.0);
+
+        let mut mesh = Mesh::new(PrimitiveTopology::LineList);
+        mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+        mesh.set_indices(Some(Indices::U32(indices)));
+
+        let mesh_handle = meshes.add(mesh);
+
+        let child = commands
+            .spawn_bundle(MeshBundle {
+                mesh: mesh_handle,
+                render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
+                    wireframe_pipeline_handle.0.clone(),
+                )]),
+                ..Default::default()
+            })
+            .id();
+
+        commands
+            .entity(e)
+            .insert(DrawVoxelDone(child))
+            .push_children(&[child]);
+    }
+}
+
+fn generate_voxel_edges_mesh(types: &[u8; CHUNK_BUFFER_SIZE]) -> (Vec<[f32; 3]>, Vec<u32>) {
+    let mut vertices = vec![];
+
+    for (idx, type_idx) in types.iter().enumerate() {
+        // TODO: Change this to Enum or better type representation
+        if *type_idx == 0u8 {
+            continue;
+        }
+
+        let pos = to_xyz_ivec3(idx);
+
+        for side in VOXEL_SIDES {
+            let side_idx = side as usize;
+
+            for idx in VERTICES_INDICES[side_idx] {
+                let v = &VERTICES[idx];
+
+                vertices.push([
+                    v[0] + pos.x as f32,
+                    v[1] + pos.y as f32,
+                    v[2] + pos.z as f32,
+                ]);
+            }
+        }
+    }
+
+    let indices = compute_wireframe_indices(vertices.len());
+
+    (vertices, indices)
+}
+
+fn delete_chunk_voxels(
+    mut commands: Commands,
+    q: Query<(Entity, &DrawVoxelDone), Without<DrawVoxels>>,
+) {
+    for (e, draw_voxel_done) in q.iter() {
+        commands.entity(draw_voxel_done.0).despawn();
+        commands.entity(e).remove::<DrawVoxelDone>();
+    }
 }
