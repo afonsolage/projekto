22141a0092094fd8d7c04453f1c0d528fb3ae531
Afonso Lage <lage.afonso@gmail.com>
2021-08-22 16:02:05 -0300

    Fixed issue with .gitignore and debug module

---
+++ src/debug/mod.rs
@@ -0,0 +1,35 @@
+use bevy::{app::AppExit, prelude::*};
+
+pub struct DebugPlugin;
+
+impl Plugin for DebugPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_hold_est_to_exit);
+        app.add_system(hold_esc_to_exit);
+    }
+}
+
+const ESC_HOLD_TIMEOUT: f32 = 0.4;
+struct EscHolding(f32);
+
+fn setup_hold_est_to_exit(mut commands: Commands) {
+    commands.insert_resource(EscHolding(0.0));
+}
+
+fn hold_esc_to_exit(
+    mut esc_holding: ResMut<EscHolding>,
+    time: Res<Time>,
+    input_keys: Res<Input<KeyCode>>,
+    mut exit_writer: EventWriter<AppExit>,
+) {
+    if input_keys.pressed(KeyCode::Escape) {
+        esc_holding.0 += time.delta_seconds();
+
+        if esc_holding.0 >= ESC_HOLD_TIMEOUT {
+            info!("Exiting app due to ESC holding...");
+            exit_writer.send(AppExit);
+        }
+    } else {
+        esc_holding.0 = 0.0;
+    }
+}
