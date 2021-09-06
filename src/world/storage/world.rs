13df7ae04a35d1fe25e8933703d4ee5fea373e41
Afonso Lage <lage.afonso@gmail.com>
2021-09-06 10:46:28 -0300

    Added tests to storage::world

--- src/world/storage/world.rs
+++ src/world/storage/world.rs
@@ -31,22 +31,6 @@ impl World {
     // }
 }
 
-impl Index<(i32, i32, i32)> for World {
-    type Output = Chunk;
-
-    fn index(&self, index: (i32, i32, i32)) -> &Self::Output {
-        &self.chunks[&(index.into())]
-    }
-}
-
-impl IndexMut<(i32, i32, i32)> for World {
-    fn index_mut(&mut self, index: (i32, i32, i32)) -> &mut Self::Output {
-        self.chunks
-            .get_mut(&(index.into()))
-            .expect(format!("No entry {:?} found on HashMap", &index).as_str())
-    }
-}
-
 impl Index<IVec3> for World {
     type Output = Chunk;
 
@@ -60,5 +44,62 @@ impl IndexMut<IVec3> for World {
         self.chunks
             .get_mut(&index)
             .expect(format!("No entry {} found on HashMap", &index).as_str())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use bevy::math::IVec3;
+
+    use super::World;
+
+    #[test]
+    fn add() {
+        let mut world = World::default();
+        world.add(IVec3::ONE);
+    }
+
+    #[test]
+    #[should_panic]
+    fn add_duplicated() {
+        let mut world = World::default();
+        world.add(IVec3::ONE);
+        world.add(IVec3::ONE);
+    }
+
+    #[test]
+    fn index() {
+        let mut world = World::default();
+        world.add(IVec3::ONE);
+
+        assert_eq!(world[(1, 1, 1).into()].len(), super::chunk::BUFFER_SIZE);
+    }
+
+    #[test]
+    fn index_mut() {
+        let mut world = World::default();
+        world.add(IVec3::ONE);
+
+        world[(1, 1, 1).into()][0] = 1;
+        assert_eq!(world[IVec3::ONE][0], 1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_index() {
+        let mut world = World::default();
+        world.add((0, 1, 2).into());
+
+        world[IVec3::ONE].len();
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_index_mut() {
+        let mut world = World::default();
+        world.add((0, 1, 2).into());
+
+        world[IVec3::ONE][0] = 1;
+        assert_eq!(world[IVec3::ONE][0], 1);
     }
 }
