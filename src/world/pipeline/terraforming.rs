0084922db5f1e3b43515706beb93453203525b44
Afonso Lage <lage.afonso@gmail.com>
2021-09-26 06:57:18 -0300

    Added tests for chunk query

--- src/world/pipeline/terraforming.rs
+++ src/world/pipeline/terraforming.rs
@@ -17,8 +17,8 @@ impl Plugin for TerraformingPlugin {
                 super::Pipeline::Terraforming,
                 SystemSet::new()
                     .with_system(process_update_chunks_system)
-                    .with_system(some_query_system)
-                    .with_system(another_system),
+                    .with_system(test_query_system)
+                    .with_system(handle_queries_system),
             );
     }
 }
@@ -44,45 +44,26 @@ fn process_update_chunks_system(
     }
 }
 
-fn another_system(world: Res<WorldRes>, mut query_res: ResMut<ChunkQuery>) {
-    if !world.is_ready() {
-        return;
-    }
-
-    let requests = query_res.queries.drain().collect::<Vec<_>>();
-
-    for (id, chunks) in requests {
-        info!("Setting result for query {} {:?}", id, chunks);
-
-        let result = chunks
-            .iter()
-            .map(|local| (*local, world.get(*local).map(|c| c.clone())))
-            .collect::<Vec<_>>();
-
-        query_res.results.insert(id, Some(result));
-    }
-}
-
 #[derive(Debug, Default)]
 struct ChunkQuery {
-    queries: HashMap<usize, Vec<IVec3>>,
-    results: HashMap<usize, Option<Vec<(IVec3, Option<ChunkKind>)>>>,
+    requests: HashMap<usize, Vec<IVec3>>,
+    results: HashMap<usize, Vec<(IVec3, Option<ChunkKind>)>>,
     next_id: usize,
 }
 
 impl ChunkQuery {
     pub fn request(&mut self, chunks: Vec<IVec3>) -> usize {
+        assert!(
+            chunks.len() > 0,
+            "At least one chunk local should be provided."
+        );
         self.next_id += 1;
-        self.queries.insert(self.next_id, chunks);
+        self.requests.insert(self.next_id, chunks);
         self.next_id
     }
 
     pub fn fetch(&mut self, id: usize) -> Option<Vec<(IVec3, Option<ChunkKind>)>> {
-        if self.results.contains_key(&id) {
-            self.results.get_mut(&id).unwrap().take()
-        } else {
-            None
-        }
+        self.results.remove(&id)
     }
 }
 
@@ -109,7 +90,26 @@ impl<'w, 's> ChunkSystemQuery<'w, 's> {
     }
 }
 
-fn some_query_system(keyboard: Res<Input<KeyCode>>, mut query: ChunkSystemQuery) {
+fn handle_queries_system(world: Res<WorldRes>, mut queries_res: ResMut<ChunkQuery>) {
+    if !world.is_ready() {
+        return;
+    }
+
+    let requests = queries_res.requests.drain().collect::<Vec<_>>();
+
+    for (id, chunks) in requests {
+        info!("Setting result for query {} {:?}", id, chunks);
+
+        let result = chunks
+            .iter()
+            .map(|local| (*local, world.get(*local).map(|c| c.clone())))
+            .collect::<Vec<_>>();
+
+        queries_res.results.insert(id, result);
+    }
+}
+
+fn test_query_system(keyboard: Res<Input<KeyCode>>, mut query: ChunkSystemQuery) {
     while let Some(result) = query.fetch() {
         info!("Some!");
         for (local, _) in result {
@@ -121,6 +121,70 @@ fn some_query_system(keyboard: Res<Input<KeyCode>>, mut query: ChunkSystemQuery)
         return;
     }
 
+    // TODO: Add raycast, add tests and refactor current system. Should I rename this module? Maybe querying?
+
     info!("Sending query");
     query.query(vec![(0, 0, 0).into()]);
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy::utils::HashSet;
+
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn chunk_query_empty() {
+        let mut query = ChunkQuery::default();
+
+        let _ = query.request(vec![]);
+    }
+
+    #[test]
+    fn chunk_query() {
+        let mut query = ChunkQuery::default();
+        let req_id = query.request(vec![(0, 0, 0).into()]);
+
+        assert!(req_id > 0, "A non-zero valid unique ID should be returned");
+
+        assert_eq!(
+            query.fetch(req_id),
+            None,
+            "A query fetch without result should return None"
+        );
+
+        assert_eq!(
+            query.fetch(12345),
+            None,
+            "A non-existing request id should return None"
+        );
+
+        query
+            .results
+            .insert(req_id, vec![((0, 0, 0).into(), Some(ChunkKind::default()))]);
+
+        let fetch = query.fetch(req_id);
+
+        assert!(fetch.is_some());
+
+        let res = fetch.unwrap();
+
+        assert_eq!(res[0].0, (0, 0, 0).into());
+        assert!(res[0].1.is_some());
+
+        assert_eq!(
+            query.fetch(req_id),
+            None,
+            "A second fetch on same request id should return None"
+        );
+
+        let mut ids = HashSet::default();
+        for _ in 0..1000 {
+            let id = query.request(vec![(0, 1, 2).into()]);
+
+            assert!(!ids.contains(&id), "Request id should always be unique");
+            ids.insert(id);
+        }
+    }
 }
