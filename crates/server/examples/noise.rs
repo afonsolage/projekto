aceae4bab4b2ba49c707f394579bb5e7cd61df42
Afonso Lage <lage.afonso@gmail.com>
2024-03-23 20:41:52 -0300

    WIP NoiseFn tree

--- crates/server/examples/noise.rs
+++ crates/server/examples/noise.rs
@@ -1,5 +1,4 @@
 use bevy::{
-    ecs::system::EntityCommands,
     prelude::*,
     render::{
         render_asset::RenderAssetUsages,
@@ -14,74 +13,61 @@ use bevy_inspector_egui::{
     quick::{ResourceInspectorPlugin, WorldInspectorPlugin},
     InspectorOptions,
 };
-use bracket_noise::prelude::{FastNoise, FractalType};
-use projekto_server::gen::noise::create_terrain_stack;
+use noise::NoiseFn;
+use projekto_server::gen::noise::{NoiseFnSpec, NoiseStack};
 
 fn main() {
     App::new()
-        .init_resource::<Noises>()
+        .init_resource::<NoiseStackRes>()
         .add_plugins(DefaultPlugins)
         .add_plugins((
             WorldInspectorPlugin::new(),
-            ResourceInspectorPlugin::<Noises>::default(),
+            ResourceInspectorPlugin::<NoiseStackRes>::default(),
         ))
         .add_systems(Startup, setup)
         .add_systems(
             Update,
             (
                 bevy::window::close_on_esc,
-                update_noise.run_if(resource_changed::<Noises>),
+                update_noise_images.run_if(resource_changed::<NoiseStackRes>),
             ),
         )
         .run();
 }
 
-#[derive(Reflect, Default, PartialEq, Eq, Hash)]
-enum NoiseType {
-    #[default]
-    Continentalness,
-    Erosion,
-}
-
-#[derive(Reflect, InspectorOptions, Default)]
-struct NoiseSettings {
-    seed: u64,
-    size: UVec2,
-    frequency: f32,
-    fractal_octaves: i32,
-    fractal_gain: f32,
-    fractal_lacunarity: f32,
-    curve: Vec<(f32, u16)>,
-    noise: Handle<Image>,
-}
-
-#[derive(Resource, Reflect, InspectorOptions, Deref, DerefMut)]
+#[derive(Resource, Debug, Default, Reflect, InspectorOptions, Deref, DerefMut)]
 #[reflect(Resource, InspectorOptions)]
-struct Noises(HashMap<NoiseType, NoiseSettings>);
+struct NoiseStackRes(NoiseStack);
 
-impl Default for Noises {
-    fn default() -> Self {
-        let mut map = HashMap::new();
-        map.insert(
-            NoiseType::Continentalness,
-            NoiseSettings {
-                seed: 42,
-                size: UVec2::new(512, 512),
-                frequency: 1.0,
-                fractal_octaves: 14,
-                fractal_gain: 0.5,
-                fractal_lacunarity: 2.2089,
-                curve: vec![(-1.0, 50), (0.3, 100), (0.4, 150), (1.0, 150)],
-                noise: Handle::default(),
-            },
-        );
+#[derive(Component, Debug, Default, Reflect)]
+struct NoiseImage(String);
 
-        Self(map)
-    }
-}
+#[derive(Component, Debug, Default, Reflect)]
+struct NoiseConfig(String);
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+
+    let content = commands
+        .spawn((
+            NodeBundle {
+                style: Style {
+                    display: Display::Grid,
+                    grid_template_columns: vec![
+                        GridTrack::flex(1.0),
+                        GridTrack::px(5.0),
+                        GridTrack::min_content(),
+                    ],
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..Default::default()
+                },
+                background_color: Color::DARK_GRAY.into(),
+                ..Default::default()
+            },
+            Name::new("Content"),
+        ))
+        .id();
 
     commands
         .spawn((
@@ -97,7 +83,7 @@ fn setup(mut commands: Commands) {
             Name::new("Root"),
         ))
         .with_children(|parent| {
-            let mut entity_cmds = parent.spawn((
+            parent.spawn((
                 NodeBundle {
                     style: Style {
                         width: Val::Auto,
@@ -112,137 +98,115 @@ fn setup(mut commands: Commands) {
                 },
                 Name::new("Border"),
             ));
-
-            add_noise_ui_node(&mut entity_cmds);
-        });
+        })
+        .add_child(content);
 
     let stack = create_terrain_stack();
+    let mut dependencies = stack.build_dep_tree("main");
+    dependencies.reverse();
+
+    for dep in dependencies {
+        add_noise_ui_node(content, dep, &mut commands);
+    }
+
+    commands.insert_resource(NoiseStackRes(stack));
 }
 
-fn add_noise_ui_node(entity_cmds: &mut EntityCommands) {
-    entity_cmds.with_children(|parent| {
-        parent
-            .spawn((
-                NodeBundle {
-                    style: Style {
-                        display: Display::Grid,
-                        grid_template_columns: vec![
-                            GridTrack::flex(1.0),
-                            GridTrack::px(5.0),
-                            GridTrack::min_content(),
-                        ],
-                        width: Val::Percent(100.0),
-                        height: Val::Percent(100.0),
-                        ..Default::default()
-                    },
-                    background_color: Color::DARK_GRAY.into(),
+fn add_noise_ui_node(parent: Entity, name: impl ToString, commands: &mut Commands) {
+    commands.entity(parent).with_children(|parent| {
+        parent.spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Auto,
+                    height: Val::Auto,
                     ..Default::default()
                 },
-                Name::new("Content"),
-            ))
-            .with_children(|parent| {
-                parent.spawn((
-                    NodeBundle {
-                        style: Style {
-                            width: Val::Auto,
-                            height: Val::Auto,
-                            ..Default::default()
-                        },
-                        background_color: Color::DARK_GREEN.into(),
+                background_color: Color::DARK_GREEN.into(),
+                ..Default::default()
+            },
+            Name::new("Noise Settings"),
+            NoiseConfig(name.to_string()),
+        ));
+
+        parent.spawn((
+            NodeBundle {
+                style: Style {
+                    height: Val::Auto,
+                    ..Default::default()
+                },
+                background_color: Color::GRAY.into(),
+                ..Default::default()
+            },
+            Name::new("Noise Spacer"),
+        ));
+
+        parent.spawn((
+            NodeBundle {
+                style: Style {
+                    width: Val::Px(256.0),
+                    height: Val::Px(256.0),
+                    ..Default::default()
+                },
+                background_color: Color::WHITE.into(),
+                ..Default::default()
+            },
+            Name::new("Noise Image"),
+            NoiseImage(name.to_string()),
+        ));
+
+        for _ in 0..3 {
+            parent.spawn((
+                NodeBundle {
+                    style: Style {
+                        height: Val::Px(2.0),
+                        width: Val::Auto,
                         ..Default::default()
                     },
-                    Name::new("Noise Settings"),
-                ));
-                parent.spawn((
-                    NodeBundle {
-                        style: Style {
-                            height: Val::Auto,
-                            ..Default::default()
-                        },
-                        background_color: Color::GRAY.into(),
-                        ..Default::default()
-                    },
-                    Name::new("Noise Spacer"),
-                ));
-                parent.spawn((
-                    NodeBundle {
-                        style: Style {
-                            width: Val::Px(300.0),
-                            height: Val::Px(300.0),
-                            ..Default::default()
-                        },
-                        background_color: Color::WHITE.into(),
-                        ..Default::default()
-                    },
-                    Name::new("Noise Settings"),
-                ));
-            });
+                    background_color: Color::GRAY.into(),
+                    ..Default::default()
+                },
+                Name::new("Noise Div"),
+            ));
+        }
     });
 }
 
-#[derive(Component, Default, Debug)]
-struct Nothing;
-
-fn update_noise(
+fn update_noise_images(
     mut commands: Commands,
-    q: Query<Entity, With<Nothing>>,
+    q: Query<(Entity, &NoiseImage), With<NoiseImage>>,
     mut images: ResMut<Assets<Image>>,
-    settings: Res<Noises>,
+    settings: Res<NoiseStackRes>,
 ) {
-    let Some(settings) = settings.get(&NoiseType::Continentalness) else {
-        return;
-    };
+    info!("Updating noise images!");
 
-    if settings.size.x == 0 || settings.size.y == 0 {
-        return;
-    }
+    for (entity, NoiseImage(name)) in &q {
+        info!("Building noise {name}");
+        let noise = settings.build(name);
+        info!("Built!");
 
-    info!("Updating noise!");
+        // 512w, 512h, 1 byte per color, 4 color channel (RGBA)
+        let mut buffer = vec![0; 4 * 512 * 512];
 
-    let Ok(entity) = q.get_single() else {
-        return;
-    };
+        info!("Rendering!");
+        for w in 0..512 {
+            for h in 0..512 {
+                let i = ((w * 512 + h) * 4) as usize;
+                let b = &mut buffer[i..i + 4];
 
-    let mut noise = FastNoise::seeded(settings.seed);
-    noise.set_noise_type(bracket_noise::prelude::NoiseType::SimplexFractal);
-    noise.set_frequency(settings.frequency);
-    noise.set_fractal_type(FractalType::FBM);
-    noise.set_fractal_octaves(settings.fractal_octaves);
-    noise.set_fractal_gain(settings.fractal_gain);
-    noise.set_fractal_lacunarity(settings.fractal_lacunarity);
+                let noise = noise.get([w as f64, 0.0, h as f64]) as f32;
+                let height = (((noise + 1.0) / 2.0) * 255.0) as u8;
 
-    // 512w, 512h, 1 byte per color, 4 color channel (RGBA)
-    let mut buffer = vec![0; 4 * 512 * 512];
-
-    let mut min = f32::MAX;
-    let mut max = 0.0;
-
-    for w in 0..512 {
-        for h in 0..512 {
-            let i = ((w * 512 + h) * 4) as usize;
-            let b = &mut buffer[i..i + 4];
-
-            let x = w as f32 / settings.size.x as f32;
-            let y = h as f32 / settings.size.y as f32;
-            let noise = noise.get_noise(x, y);
-
-            if noise < min {
-                min = noise;
-            } else if noise > max {
-                max = noise;
+                b.copy_from_slice(&[height, height, height, height]);
             }
-
-            let height = (((noise + 1.0) / 2.0) * 255.0) as u8;
-
-            b.copy_from_slice(&[height, height, height, height]);
         }
+        info!("Rendered!");
+
+        let handle = images.add(create_image(512, 512, buffer));
+
+        commands.entity(entity).insert(UiImage::new(handle));
     }
 
-    info!("Noise ({min}, {max})");
-
-    let handle = images.add(create_image(512, 512, buffer));
-
-    commands.entity(entity).insert(UiImage::new(handle));
+    info!("Updated!");
 }
 
 fn create_image(width: u32, height: u32, buffer: Vec<u8>) -> Image {
@@ -265,4 +229,65 @@ fn create_image(width: u32, height: u32, buffer: Vec<u8>) -> Image {
         asset_usage: RenderAssetUsages::RENDER_WORLD,
         ..Default::default()
     }
+}
+
+pub fn create_terrain_stack() -> NoiseStack {
+    // TODO: Move this to a ron file
+    let mut map = HashMap::new();
+
+    let continent = NoiseFnSpec::Fbm {
+        seed: 42,
+        frequency: 1.0,
+        octaves: 14,
+        lacunarity: 2.2089,
+        persistence: 0.5,
+    };
+    map.insert("continent".to_string(), continent);
+
+    let curve = NoiseFnSpec::Curve {
+        source: "continent".to_string(),
+        control_points: vec![
+            (-2.0000, -1.625),
+            (-1.0000, -1.375),
+            (0.0000, -0.375),
+            (0.0625, 0.125),
+            (0.1250, 0.250),
+            (0.2500, 1.000),
+            (0.5000, 0.250),
+            (0.7500, 0.250),
+            (1.0000, 0.500),
+            (2.0000, 0.500),
+        ],
+    };
+    map.insert("curve".to_string(), curve);
+
+    let carver = NoiseFnSpec::Fbm {
+        seed: 42,
+        frequency: 4.3437,
+        octaves: 11,
+        lacunarity: 2.2089,
+        persistence: 0.5,
+    };
+    map.insert("carver".to_string(), carver);
+
+    let scaled_carver = NoiseFnSpec::ScaleBias {
+        source: "carver".to_string(),
+        scale: 0.375,
+        bias: 0.625,
+    };
+    map.insert("scaled_carver".to_string(), scaled_carver);
+
+    let carved_continent = NoiseFnSpec::Min {
+        source_1: "scaled_carver".to_string(),
+        source_2: "curve".to_string(),
+    };
+    map.insert("carved_continent".to_string(), carved_continent);
+
+    let clamp = NoiseFnSpec::Clamp {
+        source: "carved_continent".to_string(),
+        bounds: (-1.0, 1.0),
+    };
+    map.insert("main".to_string(), clamp);
+
+    NoiseStack::new(map)
 }
--- crates/server/src/gen/noise.rs
+++ crates/server/src/gen/noise.rs
@@ -1,7 +1,7 @@
 use bevy::{reflect::Reflect, utils::HashMap};
-use noise::{Cache, Clamp, Curve, Fbm, Min, MultiFractal, NoiseFn, Perlin, ScaleBias};
+use noise::{Clamp, Curve, Fbm, Min, MultiFractal, NoiseFn, Perlin, ScaleBias};
 
-type BoxedNoiseFn = Box<dyn NoiseFn<f64, 3>>;
+pub type BoxedNoiseFn = Box<dyn NoiseFn<f64, 3> + Send>;
 
 #[derive(Debug, Clone, Reflect)]
 pub enum NoiseFnSpec {
@@ -46,9 +46,35 @@ impl NoiseFnSpec {
             NoiseFnSpec::Min { source_1, source_2 } => vec![source_1, source_2],
         }
     }
+}
 
-    fn instanciate(&self, map: &HashMap<String, NoiseFnSpec>) -> Box<dyn NoiseFn<f64, 3>> {
-        match self {
+#[derive(Debug, Default, Reflect, Clone)]
+pub struct NoiseStack {
+    spec_map: HashMap<String, NoiseFnSpec>,
+}
+
+impl NoiseStack {
+    pub fn new(spec_map: HashMap<String, NoiseFnSpec>) -> Self {
+        Self { spec_map }
+    }
+
+    pub fn build_dep_tree<'a, 'b: 'a>(&'a self, name: &'b str) -> Vec<&'a str> {
+        let spec = self.spec_map.get(name).unwrap();
+        let dependencies = spec.dependencies();
+
+        std::iter::once(name)
+            .chain(
+                dependencies
+                    .into_iter()
+                    .flat_map(|name| self.build_dep_tree(name)),
+            )
+            .collect::<Vec<&str>>()
+    }
+
+    pub fn build(&self, name: &str) -> BoxedNoiseFn {
+        let spec = self.spec_map.get(name).unwrap();
+
+        match spec {
             NoiseFnSpec::Fbm {
                 seed,
                 frequency,
@@ -67,7 +93,7 @@ impl NoiseFnSpec {
                 source,
                 control_points,
             } => {
-                let source = map.get(source).unwrap().instanciate(map);
+                let source = self.build(source);
                 let curve = control_points
                     .iter()
                     .copied()
@@ -81,117 +107,22 @@ impl NoiseFnSpec {
                 scale,
                 bias,
             } => {
-                let source = map.get(source).unwrap().instanciate(map);
+                let source = self.build(source);
                 Box::new(ScaleBias::new(source).set_scale(*scale).set_bias(*bias))
             }
             NoiseFnSpec::Min { source_1, source_2 } => {
-                let source_1 = map.get(source_1).unwrap().instanciate(map);
-                let source_2 = map.get(source_2).unwrap().instanciate(map);
+                let source_1 = self.build(source_1);
+                let source_2 = self.build(source_2);
                 Box::new(Min::new(source_1, source_2))
             }
             NoiseFnSpec::Clamp { source, bounds } => {
-                let source = map.get(source).unwrap().instanciate(map);
+                let source = self.build(source);
                 Box::new(Clamp::new(source).set_bounds(bounds.0, bounds.1))
             }
         }
     }
-}
 
-pub struct NoiseStack {
-    seed: u32,
-    map: HashMap<String, NoiseFnSpec>,
-    noise_name: String,
-    noise_fn: BoxedNoiseFn,
-}
-
-impl NoiseStack {
-    fn new(name: &str, seed: u32, spec_map: HashMap<String, NoiseFnSpec>) -> Self {
-        let noise_fn = spec_map.get(name).unwrap().instanciate(&spec_map);
-        Self {
-            seed,
-            map: spec_map,
-            noise_name: name.to_string(),
-            noise_fn,
-        }
+    pub fn main(&self) -> BoxedNoiseFn {
+        self.build("main")
     }
-
-    fn build_dep_tree<'a, 'b: 'a>(&'a self, name: &'b str) -> Vec<&'a str> {
-        let spec = self.map.get(name).unwrap();
-        let dependencies = spec.dependencies();
-
-        std::iter::once(name)
-            .chain(
-                dependencies
-                    .into_iter()
-                    .flat_map(|name| self.build_dep_tree(name)),
-            )
-            .collect::<Vec<&str>>()
-    }
-
-    pub fn get(&self, x: f64, z: f64) -> f64 {
-        self.noise_fn.get([x, 0.0, z])
-    }
-}
-
-pub fn create_terrain_stack() -> NoiseStack {
-    let mut map = HashMap::new();
-
-    let continent = NoiseFnSpec::Fbm {
-        seed: 42,
-        frequency: 1.0,
-        octaves: 14,
-        lacunarity: 2.2089,
-        persistence: 0.5,
-    };
-    map.insert("continent".to_string(), continent);
-
-    let curve = NoiseFnSpec::Curve {
-        source: "continent".to_string(),
-        control_points: vec![
-            (-2.0000, -1.625),
-            (-1.0000, -1.375),
-            (0.0000, -0.375),
-            (0.0625, 0.125),
-            (0.1250, 0.250),
-            (0.2500, 1.000),
-            (0.5000, 0.250),
-            (0.7500, 0.250),
-            (1.0000, 0.500),
-            (2.0000, 0.500),
-        ],
-    };
-    map.insert("curve".to_string(), curve);
-
-    let carver = NoiseFnSpec::Fbm {
-        seed: 42,
-        frequency: 4.3437,
-        octaves: 11,
-        lacunarity: 2.2089,
-        persistence: 0.5,
-    };
-    map.insert("carver".to_string(), carver);
-
-    let scaled_carver = NoiseFnSpec::ScaleBias {
-        source: "carver".to_string(),
-        scale: 0.375,
-        bias: 0.625,
-    };
-    map.insert("scaled_carver".to_string(), scaled_carver);
-
-    let carved_continent = NoiseFnSpec::Min {
-        source_1: "scaled_carver".to_string(),
-        source_2: "curve".to_string(),
-    };
-    map.insert("carved_continent".to_string(), carved_continent);
-
-    let clamp = NoiseFnSpec::Clamp {
-        source: "carved_continent".to_string(),
-        bounds: (-1.0, 1.0),
-    };
-    map.insert("clamp".to_string(), clamp);
-
-    let stack = NoiseStack::new("clamp", 42, map);
-    let dependency_tree = stack.build_dep_tree("clamp");
-    dbg!(dependency_tree);
-    stack
 }
